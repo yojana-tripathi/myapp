c956659e2e8f8f88d362e83cc5e4b0da2fa9dc89
yojana-tripathi <tripathiyojana640@gmail.com>
2022-06-01 16:59:53 +0530

    used app and create student

---
+++ routes/teacher.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+ // res.render('index', { title: 'Express' });
+ console.log(req)
+ res.json({name:"yojana"});
+});
+
+router.get('/:da', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    console.log(req.params.da)
+    res.json({name:req.params.da});
+   });
+   
+
+router.get('/:da-:ta', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    console.log(req.params.da)
+    res.json({name:req.params.da +" and "+req.params.ta+"are brother and sister"});
+   });
+
+router.get('/:da-:ta-ch', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    console.log(req.params.ch)
+    res.json({name:req.params.da +","+ req.params.ch +"and"+req.params.ta+"are friend"});
+   });
+
+module.exports = router;*/
