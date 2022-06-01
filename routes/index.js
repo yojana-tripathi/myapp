df6288126c7706eb15ac3d6abca35e6054b5dddc
yojana-tripathi <tripathiyojana640@gmail.com>
2022-06-01 10:19:24 +0530

    routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ // res.render('index', { title: 'Express' });
+ console.log(req)
+ res.json({name:"yojana"});
 });
 
 module.exports = router;
